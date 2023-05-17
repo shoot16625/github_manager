7f9069978ddae86d99331d0b48b652b99fb623da
uchidash <uchidash456@gmail.com>
2023-05-18 01:39:28 +0900

    add repo

---
+++ repository/homebrew-tap/main.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+set -u
+
+readonly OWNER_NAME=shoot16625
+readonly REPO_NAME=homebrew-tap
+readonly LICENSE=MIT
+readonly DEFAULT_BRANCH=main
+readonly DESCRIPTION="homebrew repository"
+readonly TOPICS="homebrew"
+
+# create repo
+gh repo create $REPO_NAME --add-readme --public --license $LICENSE
+
+# update repo
+gh repo edit $OWNER_NAME/$REPO_NAME --add-topic $TOPICS --default-branch $DEFAULT_BRANCH --delete-branch-on-merge --description "$DESCRIPTION"
+
+# create a branch protection rule
+repositoryId=$(gh api graphql -f query='
+{
+	repository(owner:"'$OWNER_NAME'", name:"'$REPO_NAME'"){id}
+}
+' -q .data.repository.id)
+
+_=$(gh api graphql -f query='
+mutation($repositoryId:ID!,$branch:String!) {
+	createBranchProtectionRule(input: {
+		repositoryId: $repositoryId
+		pattern: $branch
+	}) { clientMutationId }
+}
+' -f repositoryId="$repositoryId" -f branch="$DEFAULT_BRANCH")
+
+# update a branch protection rule
+branchProtectionRuleId=$(gh api graphql -f query='
+{
+	repository(owner:"'$OWNER_NAME'", name:"'$REPO_NAME'"){
+		branchProtectionRules(first:100){
+			nodes{
+				id,
+				pattern
+			}
+		}
+	}
+}
+' -q ' .data.repository.branchProtectionRules.nodes.[] | select(.pattern=="'$DEFAULT_BRANCH'") | .id ')
+
+_=$(gh api graphql -f query='
+mutation($branchProtectionRuleId:ID!) {
+	updateBranchProtectionRule(input: {
+		branchProtectionRuleId: $branchProtectionRuleId
+		requiresApprovingReviews: true
+		requiredApprovingReviewCount: 1
+		requiresCodeOwnerReviews: true
+		isAdminEnforced: false
+	}) { clientMutationId }
+}
+' -f branchProtectionRuleId="$branchProtectionRuleId")
